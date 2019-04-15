644d4f19aff441d01f2c0905910f15be440647e9
Gregg Anderson <gregory.anderson1@wipro.com>
2019-04-15 10:45:20 -0600

    refactor(cdn_publish.sh): fix typo

--- scripts/cdn_publish.sh
+++ scripts/cdn_publish.sh
@@ -13,4 +13,4 @@ file_contents=$(<./cdn-index.template)
 echo "${file_contents//\{versionlist\}/$versionList}" > cdn-dist/gravity/index.html
 
 ~/.local/bin/aws s3 sync cdn-dist s3://${CDN_BUCKET} --region=${PROD_BUCKET_REGION} --exclude "*" --include "*.html" --content-type "text/html; charset=utf-8" --cache-control "max-age=0"
-~/.local/bin/aws s3 sync cdn-dist s3://${CDN_BUCKET} --region=${PROD_BUCKET_REGION} --exclude "*.html" --cache-control "max-age=3153600000, immutable"l
+~/.local/bin/aws s3 sync cdn-dist s3://${CDN_BUCKET} --region=${PROD_BUCKET_REGION} --exclude "*.html" --cache-control "max-age=3153600000, immutable"
