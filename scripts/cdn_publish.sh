61a686e12837d974182cc72e226f06fca9d3a732
Gregg Anderson <gregory.anderson1@wipro.com>
2019-04-15 10:30:58 -0600

    ci(cdn_publish.sh): implement PR requested changes
    upload all files in ui-lib and add cache-control to them

--- scripts/cdn_publish.sh
+++ scripts/cdn_publish.sh
@@ -2,8 +2,7 @@
 version="$(git describe --abbrev=0 --tags)"
 mkdir -p cdn-dist/gravity/${version}
 
-cp dist/ui-lib/*.css cdn-dist/gravity/${version}
-cp dist/ui-lib/*.js cdn-dist/gravity/${version}
+cp dist/ui-lib/* cdn-dist/gravity/${version}
 
 versionList=""
 for version in $(git tag -l --sort=-committerdate); do
@@ -13,6 +12,5 @@ done
 file_contents=$(<./cdn-index.template)
 echo "${file_contents//\{versionlist\}/$versionList}" > cdn-dist/gravity/index.html
 
-~/.local/bin/aws s3 sync cdn-dist s3://${CDN_BUCKET} --region=${PROD_BUCKET_REGION} --exclude "*" --include "*.html" --content-type "text/html; charset=utf-8"
-~/.local/bin/aws s3 sync cdn-dist s3://${CDN_BUCKET} --region=${PROD_BUCKET_REGION} --exclude "*" --include "*.css" --content-type "text/css; charset=utf-8"
-~/.local/bin/aws s3 sync cdn-dist s3://${CDN_BUCKET} --region=${PROD_BUCKET_REGION} --exclude "*" --include "*.js" --content-type "application/javascript; charset=utf-8"
+~/.local/bin/aws s3 sync cdn-dist s3://${CDN_BUCKET} --region=${PROD_BUCKET_REGION} --exclude "*" --include "*.html" --content-type "text/html; charset=utf-8" --cache-control "max-age=0"
+~/.local/bin/aws s3 sync cdn-dist s3://${CDN_BUCKET} --region=${PROD_BUCKET_REGION} --exclude "*.html" --cache-control "max-age=3153600000, immutable"l
