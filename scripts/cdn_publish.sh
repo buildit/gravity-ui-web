9435aa42942324e92f34dfd6f4f74eef52a73f2f
Gregg Anderson <gregory.anderson1@wipro.com>
2019-04-18 08:51:37 -0600

    ci(cdn_publish.sh): change cdn stage dir
    changed the director name that is published to s3

--- scripts/cdn_publish.sh
+++ scripts/cdn_publish.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+cdnGravityDir="gravity-ui-web"
+cdnDir="cdn-dist"
 version="$(git describe --abbrev=0 --tags)"
-mkdir -p cdn-dist/gravity/${version}
+mkdir -p ${cdnDir}/${cdnGravityDir}/${version}
 
-cp dist/ui-lib/* cdn-dist/gravity/${version}
+cp dist/ui-lib/* ${cdnDir}/${cdnGravityDir}/${version}
 
 versionList=""
 for version in $(git tag -l --sort=-committerdate); do
@@ -10,7 +12,7 @@ for version in $(git tag -l --sort=-committerdate); do
 done
 
 file_contents=$(<./cdn-index.template)
-echo "${file_contents//\{versionlist\}/$versionList}" > cdn-dist/gravity/index.html
+echo "${file_contents//\{versionlist\}/$versionList}" > ${cdnDir}/${cdnGravityDir}/index.html
 
-~/.local/bin/aws s3 sync cdn-dist s3://${CDN_BUCKET} --region=${PROD_BUCKET_REGION} --exclude "*" --include "*.html" --content-type "text/html; charset=utf-8" --cache-control "max-age=0"
-~/.local/bin/aws s3 sync cdn-dist s3://${CDN_BUCKET} --region=${PROD_BUCKET_REGION} --exclude "*.html" --exclude ".DS_Store" --cache-control "max-age=3153600000, immutable"
+aws s3 sync ${cdnDir} s3://${CDN_BUCKET} --region=${PROD_BUCKET_REGION} --exclude "*" --include "*.html" --content-type "text/html; charset=utf-8" --cache-control "max-age=0"
+aws s3 sync ${cdnDir} s3://${CDN_BUCKET} --region=${PROD_BUCKET_REGION} --exclude "*.html" --exclude ".DS_Store" --cache-control "max-age=3153600000, immutable"
