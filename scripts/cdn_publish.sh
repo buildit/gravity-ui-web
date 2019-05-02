2f498fe9c7983653de8388f3994e631f150a1252
Gregg Anderson <gregory.anderson1@wipro.com>
2019-05-02 13:07:30 -0600

    chore(cdn_publish.sh): add latest and latest major dirs
    ISSUES CLOSED: #237

--- scripts/cdn_publish.sh
+++ scripts/cdn_publish.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 cdnGravityDir="gravity-ui-web"
-cdnDir="cdn-dist"
+cdnDistDir="cdn-dist"
+cdnCurrentVersionDir="$cdnDistDir/currentVersion"
+cdnCurrentVersionGravityDir="$cdnCurrentVersionDir/$cdnGravityDir"
+cdnLatestVersionsDir="$cdnDistDir/latestsVersions"
+cdnLatestVersionsGravityDir="$cdnLatestVersionsDir/$cdnGravityDir"
 
 if [ ! -z "$TRAVIS_COMMIT_RANGE" ]
 then
@@ -16,23 +20,34 @@ then
   version=`echo $allVersions | cut -d" " -f1`
   # be sure we get only the version part and discard any prefix
   version=`expr $version : '.*\(v[0-9].*\)'`
-
-  mkdir -p ${cdnDir}/${cdnGravityDir}/${version}
-
-  cp packages/gravity-ui-web/dist/ui-lib/* ${cdnDir}/${cdnGravityDir}/${version}
+  
+  mkdir -p ${cdnCurrentVersionGravityDir}/${version}
+  cp packages/gravity-ui-web/dist/ui-lib/* ${cdnCurrentVersionGravityDir}/${version}
 
   versionList=""
   for thisVersion in $allVersions; do
     versionList+="<li>`expr $thisVersion : '.*\(v[0-9].*\)'`</li>"
   done
-
+  
   file_contents=$(<./cdn-index.template)
-  echo "${file_contents//\{versionlist\}/$versionList}" > ${cdnDir}/${cdnGravityDir}/index.html
+  echo "${file_contents//\{versionlist\}/$versionList}" > ${cdnCurrentVersionGravityDir}/index.html
 
-  ~/.local/bin/aws s3 sync ${cdnDir} s3://${CDN_BUCKET} --region=${PROD_BUCKET_REGION} --exclude "*" --include "*.html" --content-type "text/html; charset=utf-8" --cache-control "max-age=0"
-  ~/.local/bin/aws s3 sync ${cdnDir} s3://${CDN_BUCKET} --region=${PROD_BUCKET_REGION} --exclude "*" --include "*.css" --content-type "text/css; charset=utf-8" --cache-control "max-age=3153600000, immutable" 
-  ~/.local/bin/aws s3 sync ${cdnDir} s3://${CDN_BUCKET} --region=${PROD_BUCKET_REGION} --exclude "*" --include "*.js" --content-type "application/javascript; charset=utf-8" --cache-control "max-age=3153600000, immutable"
-  ~/.local/bin/aws s3 sync ${cdnDir} s3://${CDN_BUCKET} --region=${PROD_BUCKET_REGION} --include "*" --exclude "*.js" --exclude "*.html" --exclude "*.css" --exclude ".DS_Store" --cache-control "max-age=3153600000, immutable" 
+  ~/.local/bin/aws s3 sync ${cdnCurrentVersionDir} s3://${CDN_BUCKET} --region=${PROD_BUCKET_REGION} --exclude "*" --include "*.html" --content-type "text/html; charset=utf-8" --cache-control "max-age=0"
+  ~/.local/bin/aws s3 sync ${cdnCurrentVersionDir} s3://${CDN_BUCKET} --region=${PROD_BUCKET_REGION} --exclude "*" --include "*.css" --content-type "text/css; charset=utf-8" --cache-control "max-age=3153600000, immutable" 
+  ~/.local/bin/aws s3 sync ${cdnCurrentVersionDir} s3://${CDN_BUCKET} --region=${PROD_BUCKET_REGION} --exclude "*" --include "*.js" --content-type "application/javascript; charset=utf-8" --cache-control "max-age=3153600000, immutable"
+  ~/.local/bin/aws s3 sync ${cdnCurrentVersionDir} s3://${CDN_BUCKET} --region=${PROD_BUCKET_REGION} --include "*" --exclude "*.js" --exclude "*.html" --exclude "*.css" --cache-control "max-age=3153600000, immutable" 
+
+  majorVersion=`echo $version | cut -dv -f2 | cut -d. -f1`
+
+  mkdir -p ${cdnLatestVersionsGravityDir}/${majorVersion}.x
+  cp packages/gravity-ui-web/dist/ui-lib/* ${cdnLatestVersionsGravityDir}/${majorVersion}.x
+  mkdir -p ${cdnLatestVersionsGravityDir}/latest
+  cp packages/gravity-ui-web/dist/ui-lib/* ${cdnLatestVersionsGravityDir}/latest
+
+  ~/.local/bin/aws s3 sync ${cdnLatestVersionsDir} s3://${CDN_BUCKET} --region=${PROD_BUCKET_REGION} --exclude "*" --include "*.css" --content-type "text/css; charset=utf-8" --cache-control "max-age=604800" 
+  ~/.local/bin/aws s3 sync ${cdnLatestVersionsDir} s3://${CDN_BUCKET} --region=${PROD_BUCKET_REGION} --exclude "*" --include "*.js" --content-type "application/javascript; charset=utf-8" --cache-control "max-age=604800"
+  ~/.local/bin/aws s3 sync ${cdnLatestVersionsDir} s3://${CDN_BUCKET} --region=${PROD_BUCKET_REGION} --include "*" --exclude "*.js" --exclude "*.html" --exclude "*.css" --cache-control "max-age=604800" 
+
 else 
   echo "No CDN related changes found to publish"
 fi
