6201779bfc7abdb461a9ddd77ec23590ed6bcfe7
James Nash <james.nash@wipro.com>
2019-05-07 12:40:40 +0100

    refactor: 💡 tidies up gulp task code

--- packages/gravity-ui-nunjucks/gulp/patternlab.js
+++ packages/gravity-ui-nunjucks/gulp/patternlab.js
@@ -11,30 +11,16 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const eyeglass = require('eyeglass');
-const rename = require("gulp-rename");
-const file = require('gulp-file');
-const path = require('path');
 const argv = require('minimist')(process.argv.slice(2));
-const { colors } = require('@buildit/gravity-particles');
 
-const uiLibPaths = require('@buildit/gravity-ui-web/build-api.js');
 const pkgPaths = require('./paths.js');
-const colorSchemeTables = require('./color-scheme-tables.js');
-
-const taskNamePrefix = 'patternlab:';
+const taskNamePrefix = 'pattern-library:';
 
 //read all paths from our namespaced config file
 const config = require('../patternlab-config.json');
 const patternlab = require('@pattern-lab/core')(config);
 
 const plPaths = config.paths;
-
-
-/******************************************************
- * PRE-BUILD TASKS
- * Copies / creates any generated data or patterns that
- * are then processed by Pattern Lab in the usual way
-******************************************************/
 
 // Exported array of glob patterns that clean tasks can use to
 // delete any files generated at build-time
@@ -43,97 +29,20 @@ const generatedFileGlobs = [
   pkgPaths.pkgRootPath(plPaths.source.meta, '*.mustache')
 ];
 
-const generatedPatternsDirname = '_generated';
-generatedFileGlobs.push(pkgPaths.srcPatternsPath(generatedPatternsDirname, '*'));
-generatedFileGlobs.push(`!${pkgPaths.srcPatternsPath(generatedPatternsDirname, 'README.md')}`); // Prevent README.md from being deleted
-
-function makePreSvgSymbolsTask (allowEmpty = false) {
-  const preSvgSymbolsTask = function () {
-    return gulp.src(uiLibPaths.distPath(uiLibPaths.distSvgSymbolsFilename), { allowEmpty })
-      .pipe(rename('symbols.njk'))
-      .pipe(gulp.dest(pkgPaths.srcPatternsPath(generatedPatternsDirname)));
-  }
-  preSvgSymbolsTask.displayName = taskNamePrefix + 'pre:symbols';
-  preSvgSymbolsTask.description = 'Copies Gravity\'s symbols.svg file to the patterns folder.';
-
-  return preSvgSymbolsTask;
-};
 
 
-function placeholderSvgSymbolsTask() {
-  return file('symbols.njk', ' ', { src: true })
-    .pipe(gulp.dest(pkgPaths.srcPatternsPath(generatedPatternsDirname)));
-}
-placeholderSvgSymbolsTask.displayName = taskNamePrefix + 'placeholder:symbols';
-placeholderSvgSymbolsTask.description = 'Creates an empty symbols.svg file in the patterns folder.';
-
-
-const generatedSymbolInfoFilename = '00-svg-symbols.json';
-const generatedSymbolInfoDir = pkgPaths.srcPatternsPath('00-particles', '05-logos-and-icons');
-generatedFileGlobs.push(path.join(generatedSymbolInfoDir, generatedSymbolInfoFilename));
-
-function makePreSvgSymbolsInfoTask (allowEmpty = false) {
-  const preSvgSymbolsInfoTask = function () {
-    return gulp.src(uiLibPaths.distPath(uiLibPaths.distSvgSymbolsInfoFilename), { allowEmpty })
-      .pipe(rename(generatedSymbolInfoFilename))
-      .pipe(gulp.dest(generatedSymbolInfoDir));
-  }
-  preSvgSymbolsInfoTask.displayName = taskNamePrefix + 'pre:symbols-info';
-  preSvgSymbolsInfoTask.description = 'Copies Gravity\'s symbols.json file to the patterns folder.';
-
-  return preSvgSymbolsInfoTask;
-};
-
-
-function placeholderSvgSymbolsInfoTask() {
-  return file(generatedSymbolInfoFilename, '{}', { src: true })
-    .pipe(gulp.dest(generatedSymbolInfoDir));
-}
-placeholderSvgSymbolsInfoTask.displayName = taskNamePrefix + 'placeholder:symbols-info';
-placeholderSvgSymbolsInfoTask.description = 'Creates an empty symbols.json file in the patterns folder.';
-
-
-const generatedColorPalettesDataFilename = '01-color-palettes.json';
-const generatedColorPalettesDataDir = pkgPaths.srcPatternsPath('00-particles', '00-color');
-generatedFileGlobs.push(path.join(generatedColorPalettesDataDir, generatedColorPalettesDataFilename));
-
-function preColorPaletteDataTask () {
-  const colorPalettesData = {};
-  colorPalettesData.colors = colors;
-
-  return file(generatedColorPalettesDataFilename, JSON.stringify(colorPalettesData, null, 2), { src: true })
-    .pipe(gulp.dest(generatedColorPalettesDataDir));
-}
-
-
-const generatedColorSchemeDataFilename = '02-color-schemes.json';
-const generatedColorSchemeDataDir = pkgPaths.srcPatternsPath('00-particles', '00-color');
-generatedFileGlobs.push(path.join(generatedColorSchemeDataDir, generatedColorSchemeDataFilename));
-
-function preColorSchemeTableDataTask () {
-  const colorSchemeTableData = {};
-  colorSchemeTableData.colorSchemes = colorSchemeTables;
-
-  return file(generatedColorSchemeDataFilename, JSON.stringify(colorSchemeTableData, null, 2), { src: true })
-    .pipe(gulp.dest(generatedColorSchemeDataDir));
-}
-
-
-
-const preBuildTask = gulp.parallel(makePreSvgSymbolsTask(), makePreSvgSymbolsInfoTask(), preColorSchemeTableDataTask, preColorPaletteDataTask);
-const placeholderTask = gulp.parallel(placeholderSvgSymbolsTask, placeholderSvgSymbolsInfoTask);
 
 /******************************************************
  * STYLEGUIDE CSS TASKS
 ******************************************************/
 
-function plSassTask() {
+function buildSass() {
   return gulp.src(pkgPaths.pkgRootPath(plPaths.source.root, 'sass', 'pattern-scaffolding.scss'))
     .pipe(sass(eyeglass(sass.sync().on('error', sass.logError))))
     .pipe(gulp.dest(pkgPaths.pkgRootPath(plPaths.public.css)))
 };
-plSassTask.displayName = taskNamePrefix + 'sass';
-plSassTask.description = 'Compiles pattern library CSS files from source SASS.';
+buildSass.displayName = taskNamePrefix + 'sass:build';
+buildSass.description = 'Compiles pattern library CSS files from source SASS.';
 
 
 /******************************************************
@@ -190,22 +99,14 @@ plInstallPluginTask.displayName = taskNamePrefix + 'installplugin';
  * BUILD TASK
 ******************************************************/
 
-function plBuildSgTask() {
+function buildPatternLibrary() {
   return patternlab.build({
     watch: argv.watch,
     cleanPublic: config.cleanPublic
   });
 }
-plBuildSgTask.displayName = taskNamePrefix + 'sg:build';
-plBuildSgTask.description = 'Builds the styleguide';
-
-
-const plBuildTask = gulp.series(
-  gulp.parallel(plSassTask, preBuildTask),
-  plBuildSgTask
-);
-plBuildTask.displayName = taskNamePrefix + 'build';
-plBuildTask.description = 'Compiles the patterns and frontend, outputting to config.paths.public';
+buildPatternLibrary.displayName = taskNamePrefix + 'build';
+buildPatternLibrary.description = 'Builds the styleguide';
 
 
 /******************************************************
@@ -218,7 +119,7 @@ plBuildTask.description = 'Compiles the patterns and frontend, outputting to con
 // is up and running.
 let serverReady = false;
 
-function plServerReload() {
+function serverReload() {
   if (serverReady) {
     return patternlab.server.reload();
   }
@@ -227,8 +128,10 @@ function plServerReload() {
     return Promise.resolve();
   }
 }
+serverReload.displayName = taskNamePrefix + 'server:reload';
+serverReload.description = 'Makes the local dev server trigger a full-page reload in any connected browsers.';
 
-function plServerRefreshCss() {
+function serverRefreshCss() {
   if (serverReady) {
     return patternlab.server.refreshCSS();
   }
@@ -237,68 +140,40 @@ function plServerRefreshCss() {
     return Promise.resolve();
   }
 }
+serverRefreshCss.displayName = taskNamePrefix + 'server:refresh-css';
+serverRefreshCss.description = 'Makes the local dev server trigger a CSS reload in any connected browsers.';
 
 
 /******************************************************
  * WATCH TASKS
 ******************************************************/
 
-function plWatchSvgSymbols(done) {
-  gulp.watch(
-    uiLibPaths.distPath(uiLibPaths.distSvgSymbolsFilename),
-    gulp.series(makePreSvgSymbolsInfoTask(true), plServerReload)
-  );
-  done()
-}
-plWatchSvgSymbols.displayName = taskNamePrefix + 'symbols-info:watch';
-plWatchSvgSymbols.description = 'Watches for changes to Gravity SVG symbols';
-
-function plWatchSvgSymbolsInfo(done) {
-  gulp.watch(
-    uiLibPaths.distPath(uiLibPaths.distSvgSymbolsInfoFilename),
-    gulp.series(makePreSvgSymbolsTask(true), plServerReload)
-  );
-  done()
-}
-plWatchSvgSymbolsInfo.displayName = taskNamePrefix + 'symbols:watch';
-plWatchSvgSymbolsInfo.description = 'Watches for changes to Gravity SVG symbols info file';
-
-
-function plWatchSassTask(done) {
+function watchSass(done) {
   gulp.watch(
     pkgPaths.pkgRootPath(plPaths.source.root, 'sass', '**', '*.scss'),
-    gulp.series(plSassTask, plServerRefreshCss)
+    gulp.series(buildSass, serverRefreshCss)
   );
   done()
 }
-plWatchSassTask.displayName = taskNamePrefix + 'css:watch';
-plWatchSassTask.description = 'Watches for changes to styleguide SASS and compiles to CSS.';
+watchSass.displayName = taskNamePrefix + 'sass:watch';
+watchSass.description = 'Watches for changes to styleguide SASS and compiles to CSS.';
 
 
-function plWatchSgTask() {
+function watchPatternLibrary() {
   return patternlab.build({
     watch: true,
     cleanPublic: config.cleanPublic
   });
 }
-plWatchSgTask.displayName = taskNamePrefix + 'sg:watch';
-plWatchSgTask.description = 'Watches for changes to styleguide source files.';
-
-
-const plWatchTask = gulp.series(
-  placeholderTask,
-  gulp.parallel(plSassTask, makePreSvgSymbolsTask(true), makePreSvgSymbolsInfoTask(true), preColorSchemeTableDataTask, preColorPaletteDataTask),
-  gulp.parallel(plWatchSassTask, plWatchSgTask, plWatchSvgSymbols, plWatchSvgSymbolsInfo)
-);
-plWatchTask.displayName = taskNamePrefix + 'watch';
-plWatchTask.description = 'Builds the styleguide and starts watching styleguide source files.';
+watchPatternLibrary.displayName = taskNamePrefix + 'watch';
+watchPatternLibrary.description = 'Watches for changes to styleguide source files.';
 
 
 /******************************************************
  * SERVE TASK
 ******************************************************/
 
-function plServeSgTask() {
+function startServer() {
   return patternlab.server.serve({
     watch: true,
     cleanPublic: config.cleanPublic
@@ -306,24 +181,12 @@ function plServeSgTask() {
     serverReady = true;
   });
 }
-plServeSgTask.displayName = taskNamePrefix + 'sg:serve';
-plServeSgTask.description = 'Builds styleguide HTML only and launches Pattern Lab\'s built-in server.';
-
-
-const plServeTask = gulp.series(
-  placeholderTask,
-  gulp.parallel(plSassTask, makePreSvgSymbolsTask(true), makePreSvgSymbolsInfoTask(true), preColorSchemeTableDataTask, preColorPaletteDataTask),
-  gulp.parallel(plWatchSassTask, plServeSgTask, plWatchSvgSymbols, plWatchSvgSymbolsInfo)
-);
-plServeTask.displayName = taskNamePrefix + 'serve';
-plServeTask.description = 'Builds styleguide and launches Pattern Lab\'s built-in server.';
+startServer.displayName = taskNamePrefix + 'server:start';
+startServer.description = 'Builds styleguide HTML only and launches Pattern Lab\'s built-in server.';
 
 
 
 module.exports = {
-  // Pre-build tasks
-  preBuildTask,
-
   // CLI
   plVersionTask,
   plHelpTask,
@@ -333,19 +196,21 @@ module.exports = {
   plInstallPluginTask,
 
   // Build
-  plBuildTask,
+  buildSass,
+  buildPatternLibrary,
 
   // Watch tasks
-  plWatchTask,
+  watchSass,
+  watchPatternLibrary,
 
   // Serve
-  plServeTask,
+  startServer,
 
   // Generated file paths
   generatedFileGlobs,
 
   // Export Pattern Lab server reload functions, so
   // other tasks can trigger reloads.
-  plServerReload,
-  plServerRefreshCss,
+  serverReload,
+  serverRefreshCss,
 }
--- packages/gravity-ui-nunjucks/gulpfile.js
+++ packages/gravity-ui-nunjucks/gulpfile.js
@@ -11,21 +11,62 @@
 
 const gulp = require('gulp');
 const del = require('del');
+
 const pkgPaths = require('./gulp/paths.js');
-const plTasks = require('./gulp/patternlab.js');
+const patternLibraryTasks = require('./gulp/patternlab.js');
+const preBuildTasks = require('./gulp/pre-build.js');
 const gravityTasks = require('./gulp/gravity.js');
+
 
 // Define composite tasks:
 
-gulp.task('clean', function(){
+const build = gulp.series(
+  gulp.parallel(
+    preBuildTasks.makeCopySvgSymbolsTask(),
+    preBuildTasks.makeCopySvgSymbolsInfoTask(),
+    preBuildTasks.createColorSchemeData,
+    preBuildTasks.createColorPaletteData,
+    gravityTasks.makeCopyTask(),
+    patternLibraryTasks.buildSass,
+  ),
+  patternLibraryTasks.buildPatternLibrary
+);
+build.description = 'Builds the complete pattern library.';
+
+
+const serve = gulp.series(
+  preBuildTasks.placeholderTask,
+  gulp.parallel(
+    preBuildTasks.makeCopySvgSymbolsTask(true),
+    preBuildTasks.makeCopySvgSymbolsInfoTask(true),
+    preBuildTasks.createColorSchemeData,
+    preBuildTasks.createColorPaletteData,
+    gravityTasks.makeCopyTask(true),
+    patternLibraryTasks.buildSass,
+  ),
+  gulp.parallel(
+    preBuildTasks.watchSvgSymbols,
+    preBuildTasks.watchSvgSymbolsInfo,
+    gravityTasks.watch,
+    patternLibraryTasks.watchSass,
+    patternLibraryTasks.startServer,
+  )
+);
+serve.description = 'Launches the pattern library\'s local dev server, and watches all relevant source files for changes.';
+
+
+function clean(){
   return del([
     pkgPaths.pkgRootPath('dist', '**', '*'),
-    ...plTasks.generatedFileGlobs
+    ...patternLibraryTasks.generatedFileGlobs,
+    ...preBuildTasks.generatedFileGlobs
   ]);
-});
+}
+clean.description = 'Deletes all intermediated and final build output files.';
 
 
 module.exports = {
-  default: gulp.parallel(plTasks.plBuildTask, gravityTasks.copy),
-  serve: gulp.parallel(plTasks.plServeTask, gravityTasks.watch)
+  default: build,
+  serve,
+  clean
 };
