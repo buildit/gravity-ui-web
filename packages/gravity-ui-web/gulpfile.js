c5615efedb213599ece99f2f7186f9a96c74f2d4
Ash Blue <ash@blueashes.com>
2019-05-21 14:56:53 -0600

    chore(javascript): eslint now part of the linting pipeline
    affects: @buildit/gravity-ui-web

--- packages/gravity-ui-web/gulpfile.js
+++ packages/gravity-ui-web/gulpfile.js
@@ -1,4 +1,4 @@
-/******************************************************
+/** ****************************************************
  * GRAVITY UI LIB + LIVING STYLE GUIDE
  *
  * Contains tasks to:
@@ -6,8 +6,7 @@
  * - Build the UI lib
  * - Build the living style guide
  *
-******************************************************/
-
+***************************************************** */
 
 const gulp = require('gulp');
 const del = require('del');
@@ -16,15 +15,13 @@ const uiLibTasks = require('./gulp/ui-lib.js');
 
 // Define composite tasks:
 
-gulp.task('clean', function(){
-  return del([
-    pkgPaths.pkgRootPath('dist', '**', '*')
-  ]);
-});
+gulp.task('clean', () => del([
+  pkgPaths.pkgRootPath('dist', '**', '*'),
+]));
 
 gulp.task('watch', gulp.series(
   uiLibTasks.buildTasks,
-  uiLibTasks.watchTask
+  uiLibTasks.watchTask,
 ));
 
 gulp.task('default', uiLibTasks.buildTasks);
