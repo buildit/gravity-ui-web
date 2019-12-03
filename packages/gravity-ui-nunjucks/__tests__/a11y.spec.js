a1f0c175e8d1e1eb5ddfe96e38258b4e906cd286
Elohina Guevara <elohinaguevara@gmail.com>
2019-12-03 14:48:13 +0000

    refactor(a11y): improve browser launching before tests
    affects: @buildit/gravity-ui-nunjucks
    
    ISSUES CLOSED: #361

--- packages/gravity-ui-nunjucks/__tests__/a11y.spec.js
+++ packages/gravity-ui-nunjucks/__tests__/a11y.spec.js
@@ -6,16 +6,26 @@ const { excludedA11yFiles, excludedA11yRules } = require('../test-consts');
 
 describe('A11y tests', () => {
   const componentNames = getComponentsNames(excludedA11yFiles);
+  let browser;
+  let page;
+
+  beforeAll(async (done) => {
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+    await page.setBypassCSP(true);
+    done();
+  });
+
+  afterAll(async (done) => {
+    await browser.close();
+    done();
+  });
 
   componentNames.forEach((component) => {
     describe(`${component} component`, () => {
       it('should have no accessibility violations', (done) => {
         try {
           (async () => {
-            const browser = await puppeteer.launch();
-            const page = await browser.newPage();
-            await page.setBypassCSP(true);
-
             await page.goto(`${pkgEnvs.getCurrentEnvInfo().url}/components/preview/${component}`);
 
             const results = await new AxePuppeteer(page)
@@ -33,7 +43,7 @@ describe('A11y tests', () => {
                 violation.nodes.forEach((node) => (console.log('\x1b[37m', `${node.html}\n`)));
               });
             }
-            await browser.close();
+
             done();
           })();
         } catch (error) {
